b3cc6a3680156c51b9f6c0f772973e739609f6fe
haxmind <me@hackmind.ru>
2019-06-19 12:02:59 +0400

    todo-list

--- src/Todos.js
+++ src/Todos.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import { Todo } from './Todo';
+import "./Todos.css";
 
 export class Todos extends React.Component {
     constructor(props) {
         super(props);
-        const { tasks } = props; //tasks = props.tasks
+
+        const { tasks } = props; // const tasks = props.tasks;
 
         this.state = {
-            tasks: tasks || []
+            tasks: tasks || [],
+            newTaskText: ''
         };
     }
+
+    handleChange(event) {
+        this.setState({
+            newTaskText: event.target.value
+        });
+    }
+
+    handleAddTask() {
+        const { newTaskText } = this.state;
+        
+        if (newTaskText.length === 0) {
+            alert("Введите текст задачи!");
+            return;
+        }
+
+        this.setState({
+            tasks: [
+                {
+                    text: newTaskText
+                },
+                ...this.state.tasks
+            ],
+            newTaskText: ''
+        });
+    }
+
     render() {
-        const { tasks } = this.state;
-        return <div>
-             <ul>
-                {tasks.map(task => <Todo key={task.text} task={task}>{task.text}</Todo>)}
-             </ul>
+        const { tasks, newTaskText } = this.state;
+
+        return <div className="todos">
+            <input value={newTaskText} onChange={this.handleChange.bind(this)} type="text" placeholder="Введите новую задачу"></input>
+            <button onClick={this.handleAddTask.bind(this)} type="button">Добавить</button>
+            <ul>
+                {tasks.map(task => <Todo key={task.text} task={task}></Todo>)}
+            </ul>
         </div>;
     }
 }
-

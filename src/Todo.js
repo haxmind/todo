315a7f7fa7bffbd18538305d87802280b4f0e379
haxmind <me@hackmind.ru>
2019-06-20 11:51:24 +0400

    Update add ask, change add button, add task removal, add ability to mark tasks as completed

--- src/Todo.js
+++ src/Todo.js
@@ -12,9 +12,23 @@ export class Todo extends React.Component {
         };
     }
 
+    handleDelete() {
+        this.props.onDelete(this.state.task.id);
+    }
+
+    handleComplte() {
+        this.props.onComplete(this.state.task.id);
+    }
+
     render() {
         const { task } = this.state;
 
-        return <li className="todo">{task.text}</li>;
+        return <li className={"todo" + (task.completed ? " completed" : "")}>
+            <input onChange={this.handleComplte.bind(this)} type="checkbox"></input>
+            <div className="todo-text">
+                {task.text}
+            </div>
+            <i className="fas fa-trash" onClick={this.handleDelete.bind(this)}></i>
+        </li>;
     }
 }
--- src/Todos.js
+++ src/Todos.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Todo } from './Todo';
 import "./Todos.css";
 
+let idShift = 1000;
+
 export class Todos extends React.Component {
     constructor(props) {
         super(props);
@@ -20,6 +22,49 @@ export class Todos extends React.Component {
         });
     }
 
+    handleDelete(id) {
+        this.setState({
+            tasks: this.state.tasks.filter(task => task.id !== id)
+        });
+    }
+
+    handleComplete(id) {
+        const { tasks } = this.state;
+        const completedTask = tasks.find(task => task.id === id);
+
+        if (completedTask === undefined) {
+            return;
+        }
+
+        if (completedTask.completed === true) {
+            completedTask.completed = false;
+
+            this.setState({
+                tasks: [
+                    completedTask,
+                    ...tasks.filter(task => task.id !== id)
+                ]
+            });
+
+            return;
+        }
+
+        completedTask.completed = true;
+
+        this.setState({
+            tasks: [
+                ...tasks.filter(task => task.id !== id),
+                completedTask
+            ]
+        });
+    }
+
+    handleOnEnterPress(event) {
+        if (event.key === 'Enter') {
+            this.handleAddTask();
+        }
+    }
+
     handleAddTask() {
         const { newTaskText } = this.state;
         
@@ -31,6 +76,7 @@ export class Todos extends React.Component {
         this.setState({
             tasks: [
                 {
+                    id: idShift++,
                     text: newTaskText
                 },
                 ...this.state.tasks
@@ -43,10 +89,23 @@ export class Todos extends React.Component {
         const { tasks, newTaskText } = this.state;
 
         return <div className="todos">
-            <input value={newTaskText} onChange={this.handleChange.bind(this)} type="text" placeholder="Введите новую задачу"></input>
-            <button onClick={this.handleAddTask.bind(this)} type="button">Добавить</button>
+            <div className="add-todo">
+                <input 
+                    value={newTaskText}
+                    onChange={this.handleChange.bind(this)}
+                    onKeyPress={this.handleOnEnterPress.bind(this)}
+                    type="text"
+                    placeholder="Введите новую задачу"
+                ></input>
+                <i className="fas fa-plus" onClick={this.handleAddTask.bind(this)}></i>
+            </div>
             <ul>
-                {tasks.map(task => <Todo key={task.text} task={task}></Todo>)}
+                {tasks.map(task => <Todo
+                    key={task.id}
+                    task={task}
+                    onDelete={this.handleDelete.bind(this)}
+                    onComplete={this.handleComplete.bind(this)}
+                    ></Todo>)}
             </ul>
         </div>;
     }
